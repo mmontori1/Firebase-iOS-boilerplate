8141f84d1924ac3d9c1a7064540f3e9e9249fdaa
Mariano <mmontori@umich.edu>
2017-07-24 11:43:09 -0400

    Initial Commit

---
+++ Firebase-boilerplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase-boilerplate
+//
+//  Created by Mariano Montori on 7/24/17.
+//  Copyright © 2017 Mariano Montori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
