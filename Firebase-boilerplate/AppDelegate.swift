040d8a781671164c5dcef26080f2ef7bfbdd471a
Mariano <mmontori@umich.edu>
2017-07-24 16:10:19 -0400

    configure starting view

--- Firebase-boilerplate/AppDelegate.swift
+++ Firebase-boilerplate/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 //        FirebaseApp.configure()
+        configureInitialRootViewController(for: window)
         return true
     }
 
