ccbaa40619a869b74320d8ed5bfc8cfe29a5c119
Mariano <mmontori@umich.edu>
2017-07-24 13:58:49 -0400

    init

---
+++ Firebase-boilerplate/User.swift
@@ -0,0 +1,72 @@
+//
+//  User.swift
+//
+//  Created by Mariano Montori on 7/24/17.
+//  Copyright © 2017 Mariano Montori. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseDatabase.FIRDataSnapshot
+
+class User : NSObject {
+    
+    //User variables
+    let uid : String
+    let username : String
+    
+    //Standard User init()
+    init(uid: String, username: String) {
+        self.uid = uid
+        self.username = username
+        super.init()
+    }
+    
+    //User init using Firebase snapshots
+    init?(snapshot: DataSnapshot) {
+        guard let dict = snapshot.value as? [String : Any],
+            let username = dict["username"] as? String
+            else { return nil }
+        self.uid = snapshot.key
+        self.username = username
+    }
+    
+    //UserDefaults
+    required init?(coder aDecoder: NSCoder) {
+        guard let uid = aDecoder.decodeObject(forKey: "uid") as? String,
+            let username = aDecoder.decodeObject(forKey: "username") as? String
+            else { return nil }
+        
+        self.uid = uid
+        self.username = username
+    }
+    
+    
+    //User singleton for currently logged user
+    private static var _current: User?
+    
+    static var current: User {
+        guard let currentUser = _current else {
+            fatalError("Error: current user doesn't exist")
+        }
+        
+        return currentUser
+    }
+    
+    class func setCurrent(_ user: User, writeToUserDefaults: Bool = false) {
+        if writeToUserDefaults {
+            let data = NSKeyedArchiver.archivedData(withRootObject: user)
+            
+            UserDefaults.standard.set(data, forKey: "currentUser")
+        }
+        
+        _current = user
+    }
+}
+
+extension User: NSCoding {
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(uid, forKey: "uid")
+        aCoder.encode(username, forKey: "username")
+    }
+}
---
+++ Firebase-boilerplate/UserService.swift
@@ -0,0 +1,39 @@
+//
+//  UserService.swift
+//
+//  Created by Mariano Montori on 7/24/17.
+//  Copyright © 2017 Mariano Montori. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+struct UserService {
+    static func create(_ firUser: FIRUser, username: String, completion: @escaping (User?) -> Void) {
+        let userAttrs = ["username": username]
+        
+        let ref = Database.database().reference().child("users").child(firUser.uid)
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return completion(nil)
+            }
+            
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                let user = User(snapshot: snapshot)
+                completion(user)
+            })
+        }
+    }
+    
+    static func show(forUID uid: String, completion: @escaping (User?) -> Void) {
+        let ref = Database.database().reference().child("users").child(uid)
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let user = User(snapshot: snapshot) else {
+                return completion(nil)
+            }
+            
+            completion(user)
+        })
+    }
+}
